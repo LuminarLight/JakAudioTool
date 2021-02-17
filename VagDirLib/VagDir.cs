8b35659c8280b8845ab12dde4018242b89c2e71a
Luminar Light <18116946+LuminarLight@users.noreply.github.com>
2021-02-17 09:49:06 +0100

    Testing

--- VagDirLib/VagDir.cs
+++ VagDirLib/VagDir.cs
@@ -11,28 +11,31 @@ namespace VagDirLib
 
     }
 
-    /*        class VAGDIR_EntryAdvanced // Not sure how it knows whether it is in a language VAGWAD or the INT VAGWAD, but I believe it won't be hard to figure out once we know the rest.
+    class VAGDIR_EntryAdvanced // Not sure how it knows whether it is in a language VAGWAD or the INT VAGWAD, but I believe it won't be hard to figure out once we know the rest.
+    {
+        public VAGDIR_EntryAdvanced(int unknown, UInt16 unknown2, UInt16 location)
         {
-            public VAGDIR_EntryAdvanced(int unknown, UInt16 unknown2, UInt16 location)
-            {
-                Unknown = unknown;
-                Unknown2 = unknown2;
-                Location = location;
-            }
+            Unknown = unknown;
+            Unknown2 = unknown2;
+            Location = location;
+        }
 
-            public int Unknown { get; } // This could be the name but I did the math and it seems to be impossible to store a length 8 string in 4 bytes...
-            public UInt16 Unknown2 { get; } // these 2 bytes seem to have some kind of pattern...
-            public UInt16 Location { get; } // multiply this by 0x8000 to get the audio's position in the VAGWAD files
-        }*/
-    /*            public VAGDIR_EntrySimple(int unknown, string name, UInt32 location)
-            {
-                Unknown = unknown;
-                Name = name;
-                Location = location;
-            }
+        public int Unknown { get; } // This could be the name but I did the math and it seems to be impossible to store a length 8 string in 4 bytes...
+        public UInt16 Unknown2 { get; } // these 2 bytes seem to have some kind of pattern...
+        public UInt16 Location { get; } // multiply this by 0x8000 to get the audio's position in the VAGWAD files
+    }
 
-            public int Unknown { get; } // 4 bytes - strange value for the first entry, 0 or 1 for the rest
-            public string Name { get; } // always 8 bytes, if less then add space to the end of the name to reach 8 characters
-            public UInt32 Location { get; } // multiply this by 0x800 to get the audio's position in the VAGWAD files
-        }*/
+    class VAGDIR_EntrySimple
+    {
+        public VAGDIR_EntrySimple(int unknown, string name, UInt32 location)
+        {
+            Unknown = unknown;
+            Name = name;
+            Location = location;
+        }
+
+        public int Unknown { get; } // 4 bytes - strange value for the first entry, 0 or 1 for the rest
+        public string Name { get; } // always 8 bytes, if less then add space to the end of the name to reach 8 characters
+        public UInt32 Location { get; } // multiply this by 0x800 to get the audio's position in the VAGWAD files
+    }
 }
